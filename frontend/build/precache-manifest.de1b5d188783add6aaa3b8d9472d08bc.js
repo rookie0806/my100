4baf5064d3358d4fc171e9ed02d128c65ab0ad6f
kimdonghui <kpho97@gmail.com>
2019-01-22 01:08:21 +0900

    12

---
+++ frontend/build/precache-manifest.de1b5d188783add6aaa3b8d9472d08bc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "780f3f31ac709cdfddb6",
+    "url": "/static/css/main.60987dd2.chunk.css"
+  },
+  {
+    "revision": "780f3f31ac709cdfddb6",
+    "url": "/static/js/main.780f3f31.chunk.js"
+  },
+  {
+    "revision": "b9578eec70488d571a13",
+    "url": "/static/css/1.82ebf44c.chunk.css"
+  },
+  {
+    "revision": "b9578eec70488d571a13",
+    "url": "/static/js/1.b9578eec.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "667102992c39168c86b0a4939b8ae77c",
+    "url": "/static/media/logo.66710299.png"
+  },
+  {
+    "revision": "5d17c0469c60375a01091fcee28f3749",
+    "url": "/static/media/phone.5d17c046.png"
+  },
+  {
+    "revision": "f06b908907d5d4f2aaf733e2bee7ea8e",
+    "url": "/static/media/android.f06b9089.png"
+  },
+  {
+    "revision": "a6942251372772acfa468d4e1558cc2d",
+    "url": "/index.html"
+  }
+];
